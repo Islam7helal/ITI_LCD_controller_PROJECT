8fa2f1e605ec14fa9de66173e0a1e6afe2ac8afc
islam osama helal <65996367+Islam7helal@users.noreply.github.com>
2022-11-09 21:02:10 +0200

    Add files via upload

---
+++ LIB/STD_TYPES.h
@@ -0,0 +1,24 @@
+/***************************************************************************************/
+/****************************  standard types header file ******************************/
+/***************************************************************************************/
+/***************************************************************************************/
+#ifndef _STD_TYPES_H
+#define _STD_TYPES_H
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int u16;
+typedef signed short int s16;
+
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+typedef float f32;
+typedef double f64;
+
+#ifndef NULL
+#define NULL ((void *)0)
+#endif
+
+#endif
